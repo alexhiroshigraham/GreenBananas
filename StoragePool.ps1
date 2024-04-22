6468bc0eb71aba5e07d40b9d8fff251100a59d02
alexhiroshigraham <71856626+alexhiroshigraham@users.noreply.github.com>
2024-04-22 14:38:27 -0700

    Update StoragePool.ps1

--- StoragePool.ps1
+++ StoragePool.ps1
@@ -2,19 +2,21 @@
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
 #retreiving info from json file created by setup script
-$jsonFilePath = "C:\_Relion\Scripts\supabase.json"
+$jsonFilePath = "C:\GreenBananas\supabase.json"
 $jsonContent = Get-Content -Raw -Path $jsonFilePath
 $jsonObject = ConvertFrom-Json $jsonContent
 
 #defining variables for each object in json file
 $anonKey = $jsonObject.anonKey
-$tableName = $jsonObject.tableName
 $clientName = $jsonObject.clientName
 $url = $jsonObject.url
 $clientJWT = $jsonObject.clientJWT
 
 #defining device name
 $deviceName = $($env:COMPUTERNAME)
+
+#defining table name
+$tableName = "StoragePool"
 
 #defining primary key
 $primaryKey = "?ServerName=eq." + $deviceName
