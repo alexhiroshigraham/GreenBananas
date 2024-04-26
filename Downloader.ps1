fefaf781e1a83e987df2ce66dd928e4c23821bb0
alexhiroshigraham <71856626+alexhiroshigraham@users.noreply.github.com>
2024-04-25 17:52:41 -0700

    Update Downloader.ps1

--- Downloader.ps1
+++ Downloader.ps1
@@ -10,8 +10,14 @@ $storagePool = "https://github.com/alexhiroshigraham/GreenBananas/raw/main/Stora
 #defining Hyper-V Replication Health Script URL
 $hyperV = "https://github.com/alexhiroshigraham/GreenBananas/raw/main/HyperV.ps1"
 
+#defining Disk Space Script URL
+$diskSpace = "https://github.com/alexhiroshigraham/GreenBananas/raw/main/DiskSpace.ps1"
+
 #downloading Storage Pool Script from Github
 Invoke-WebRequest -Uri $storagePool -OutFile "$filePath\StoragePool.ps1"
 
 #downloading Hyper-V Replication Health Script from Github
 Invoke-WebRequest -Uri $hyperV -OutFile "$filePath\HyperV.ps1"
+
+#downloading Disk Space Script from Github
+Invoke-WebRequest -Uri $diskSpace -OutFile "$filePath\DiskSpace.ps1"
